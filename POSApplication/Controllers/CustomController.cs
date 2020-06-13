8615dcdb3ab5c442f3f7c141175496c5a851ee33
Talukder_PC <talukder.roni.ict@gmail.com>
2020-06-14 01:27:45 +0600

    Asdf

---
+++ POSApplication/Controllers/CustomController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace POSApplication.Controllers
+{
+    public class CustomController : Controller
+    {
+        // GET: Custom
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
